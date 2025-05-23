fc63d17859595e3996240a16b1072ba09ef7688e
brokemadefashion <brokemadefashion@gmail.com>
2025-05-23 20:02:52 +0530

    Setup env file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const env = require("dotenv").config()
 
 const port = 3001
 
-app.listen(port,()=>{
+app.get('/',(req,res)=>{
+    res.end('We started the project')
+})
+
+app.listen(env.port,()=>{
     console.log(`Port is running at ${port}`);
 })

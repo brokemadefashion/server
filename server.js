e86b816f1f07dc79716d863d07513bfa4a9d9c23
brokemadefashion <brokemadefashion@gmail.com>
2025-05-23 20:11:00 +0530

    Last commit before code generation

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express')
 const app = express()
 const env = require("dotenv").config()
 
-const port = 3001
 
 app.get('/',(req,res)=>{
     res.end('We started the project')
 })
 
-app.listen(env.port,()=>{
-    console.log(`Port is running at ${port}`);
+app.listen(process.env.PORT,()=>{
+    console.log(`Port is running at ${process.env.PORT}`);
 })
